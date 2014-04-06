8bbd0497a9a209988ae148d3f2cdce7e1ee29795
mubaidr <mubaidr@gmail.com>
2014-04-06 10:01:02 -0700

    First release

---
+++ js/background.js
@@ -0,0 +1,21 @@
+/* global chrome */
+chrome.pageAction.onClicked.addListener(function (tab) {
+  copyURLTest(tab.url);
+});
+
+chrome.tabs.onUpdated.addListener(function (tabId) {
+  chrome.tabs.get(tabId, function (tab) {
+    if (tab.url.substring(0,5).lastIndexOf('opera')===-1) {
+      chrome.pageAction.show(tabId);
+    }
+  });  
+});
+
+function copyURLTest(url) {
+  var textArea= document.createElement("textarea");
+  textArea.value = url;
+  document.body.appendChild(textArea);
+  textArea.select();
+  document.execCommand("copy");
+  textArea.remove();
+}
